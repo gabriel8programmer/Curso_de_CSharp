798d6432fbd0a37e37a4fcf236ba8998c52cb206
gabriel8programmer <gabriel8github@gmail.com>
2021-11-22 18:43:53 -0200

    Adicionei a pasta da aula02 e arquivos main.cs e main.exe

---
+++ Aula03/main.cs
@@ -0,0 +1,13 @@
+
+//TIPOS PRIMITIVOS E VÁRIAVEIS
+using System;
+
+class Programm{
+	
+	static void Main(){
+		string nome = "Gabriel";
+		int idade = 18;
+		Console.WriteLine("Meu nome é " + nome + " e tenho " + idade + " anos de idade!");
+	}
+	
+}
