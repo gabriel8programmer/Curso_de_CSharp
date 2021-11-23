a80df3bad4df864cca3a1b7034f7430798b1c508
gabriel8programmer <gabriel8github@gmail.com>
2021-11-23 11:15:28 -0200

    Adicionei a pasta e os arquivos da aula06 e mudei os arquivos da aula04 e aula05

--- Aula04/main.cs
+++ Aula04/main.cs
@@ -1,60 +1,16 @@
 
-//OPERADORES E SUAS OPERAÇÕES 
+//ESCOPO DE VARIÁVEIS
 using System;
 
 class Programm{
 	
+	static int numero = 100;//variável global a classe programm
+	
 	static void Main(){
+		int numero1 = 50;//variável local ao método main
 		
-		int n1 = 10; 
-		int n2 =  5;
-		
-		Console.WriteLine("---------------------------------------------");
-		Console.WriteLine("          OPERADORES MATEMÁTICOS             ");
-		Console.WriteLine("---------------------------------------------");
-		Console.WriteLine(n1 + " + " + n2 + " = " + (n1+n2) );
-		Console.WriteLine(n1 + " - " + n2 + " = " + (n1-n2) );
-		Console.WriteLine(n1 + " * " + n2 + " = " + (n1*n2) );
-		Console.WriteLine(n1 + " / " + n2 + " = " + (n1/n2) );
-		Console.WriteLine(n1 + " % " + n2 + " = " + (n1%n2) );
-		Console.WriteLine("---------------------------------------------\n");
-		
-		Console.WriteLine("---------------------------------------------");
-		Console.WriteLine("          OPERADORES RELACIONAIS             ");
-		Console.WriteLine("---------------------------------------------");
-		Console.WriteLine(n1 + " >  " + n2 + " = " + (n1>n2) );
-		Console.WriteLine(n1 + " <  " + n2 + " = " + (n1<n2) );
-		Console.WriteLine(n1 + " >= " + n2 + " = " + (n1>=n2) );
-		Console.WriteLine(n1 + " <= " + n2 + " = " + (n1<=n2) );
-		Console.WriteLine(n1 + " == " + n2 + " = " + (n1==n2) );
-		Console.WriteLine(n1 + " != " + n2 + " = " + (n1!=n2) );
-		Console.WriteLine("---------------------------------------------\n");
-		
-		Console.WriteLine("------------------------------------------------------------");
-		Console.WriteLine("          OPERADORES DE INCREMENTO E DECREMENTO             ");
-		Console.WriteLine("------------------------------------------------------------");
-		Console.WriteLine("n1++  = " +  n1++);
-		Console.WriteLine("n2--  = " +  n2--);
-		Console.WriteLine("------------------------------------------------------------");
-		
-		Console.WriteLine("--------------------------------------------------------------------");
-		Console.WriteLine("          OPERADORES DE POS-INCREMENTO E POS-DECREMENTO             ");
-		Console.WriteLine("--------------------------------------------------------------------");
-		Console.WriteLine("++n1  = " +  ++n1);
-		Console.WriteLine("++n2  = " +  --n2);
-		Console.WriteLine("--------------------------------------------------------------------");
-		
-		Console.WriteLine("---------------------------------------------");
-		Console.WriteLine("          OPERADORES LÓGICOS                 ");
-		Console.WriteLine("---------------------------------------------");
-		bool certo;
-		certo = ( n1 > n2 & ((n2 - 2) == 10));
-		Console.WriteLine("Certo = " + certo);
-		certo = ( n1 > n2 | ((n2 - 2) == 10));
-		Console.WriteLine("Certo = " + certo);
-		certo = (!((n2+n1) > 20));
-		Console.WriteLine("Certo = " + certo);
-		Console.WriteLine("---------------------------------------------\n");
+		Console.WriteLine("Número Global: " + numero);
+		Console.WriteLine("Número Local : " + numero1);
 	}
 	
 }
---
+++ Aula06/main.cs
@@ -0,0 +1,13 @@
+
+//FORMATAR A SAÍDA DO CONSOLE
+using System;
+
+class Programm{
+	
+	static void Main(){
+		string nome = "Gabriel";
+		int idade = 18;
+		
+		Console.WriteLine("Meu nome é {0} e eu tenho {1} anos de idade!", nome, idade );
+	}
+}
